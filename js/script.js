cf464d43271ddbd654419050ef5dc5a5bd1b043e
jeasika <jesika_18_@hotmail.com>
2021-07-09 17:52:07 -0600

    html, css and js added

--- js/script.js
+++ js/script.js
@@ -13,19 +13,55 @@ function TodoPost(e){
 
   var to_do = document.getElementById("todoText").value;
   var list = document.getElementById("todoList");
-
+/*
   var currentListHTML = list.innerHTML;
-  list.innerHTML = currentListHTML + `<input type= "checkbox" name="todo"/> ${to_do} <br>`
+  list.innerHTML = currentListHTML + `<input type= "checkbox" name="todo"/> ${to_do} <br>`*/
+
+  var div = document.createElement("div");
+  var checkbox = document.createElement("input");
+  var label = document.createElement("label");
+
+  checkbox.type = "checkbox";
+  checkbox.name = "todo";
+
+  label.textContent = to_do;
+
+  div.appendChild(checkbox);
+  div.appendChild(label);
+  list.appendChild(div);
+
+  to_do = "";
 }
 
 function TodoClear(){
+  var todos = document.getElementsByName("todo");
 
+  for (var i = 0; i < todos.length; i++){
+    todos[i].checked = false;
+  }
 }
 
 function TodoMark(){
 
+  var todos = document.getElementsByName("todo");
+
+  for (var i = 0; i < todos.length; i++){
+    todos[i].checked = true;
+  }
 }
 
 function TodoDel(){
+  /*var list = document.getElementById("todoList");
+  list.innerHTML = ""; */
+
+  var todos = document.getElementsByName("todo");
+
+  todos.forEach(elemento => {
+
+    if(elemento.checked){
+      elemento.parentElement.remove();
+    }
+
+  })
 
 }
